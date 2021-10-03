8f09dbe3ccc4b6529ffb1c343f2adf88f9b7a30c
Davis Ransom <davisransom.vb@gmail.com>
2021-10-02 19:14:12 -0700

    Updated file structure

--- Assets/script.js
+++ Assets/script.js
@@ -1,5 +1,9 @@
-//when the user hits a button to begin the game, a timer begins.
-//Then users are asked questions about programming.
+//when the user hits a button to begin the game, a timer begins
+//Then users are asked questions about programming
+//The user inputs their answer in a textarea
 //When a user misses a question the timer goes down by an additional 5 seconds
-
+//Every time a user gets a question correct, they add +1 to their correct score total
+//Every time a user gets a question incorrect, they add +1 to their incorrect score total
+//When the timer gets to 0, a message that says "Game Over" comes up
+//
 
