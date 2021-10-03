8205fe633f1191b110ccd1d57064d58b45e9f1c9
Davis Ransom <davisransom.vb@gmail.com>
2021-10-02 18:11:33 -0700

    Initial file setup

---
+++ Assets/script.js
@@ -0,0 +1,5 @@
+//when the user hits a button to begin the game, a timer begins.
+//Then users are asked questions about programming.
+//When a user misses a question the timer goes down by an additional 5 seconds
+
+
